daf0bed2c83107e3aae310445a358f6c74d39755
Sarju Mulmi <mulmi.sarju@gmail.com>
2019-10-03 15:19:36 -0400

    styled components added

--- src/components/Navbah.js
+++ src/components/Navbah.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link, withRouter } from 'react-router-dom'
+import styled from 'styled-components'
 import { logout } from './loginReducer'
 import { notify } from './notificationReducer'
 
+const Navbar = styled.nav`
+  display: flex;
+  background-color: #DBDBDB;
+  padding: .5rem;
+  margin-bottom: 1rem;
+`
+
 const Navbah = ({ currentUser, logout, notify, history }) => {
-  const style = {
-    backgroundColor: '#DBDBDB',
-    padding: '.5rem'
-  }
 
   const handleLogout = () => {
     notify({ msg: `User ${currentUser.username} logged out!`, type: 'success' })
@@ -17,21 +21,21 @@ const Navbah = ({ currentUser, logout, notify, history }) => {
     history.push('/login')
   }
   return (
-    <div style={style}>
+    <Navbar >
       {currentUser.username && (
-        <div>
-          <Link to="/">blogs</Link>&nbsp;
-          <Link to="/users">users</Link>&nbsp;
-          <span>{currentUser.username} logged in</span>&nbsp;
-          <button onClick={handleLogout} >logout</button>&nbsp;
-        </div>
+        <>
+          <Link to="/">Blogs</Link>&nbsp;
+          <Link to="/users">Users</Link>&nbsp;
+          <span>{currentUser.username} Logged in</span>&nbsp;
+          <button onClick={handleLogout} >Logout</button>&nbsp;
+        </>
       )}
       {!currentUser.username && (
         <div>
-          <button onClick={() => history.push('/login')} >login</button>&nbsp;
+          <button onClick={() => history.push('/login')} >Login</button>&nbsp;
         </div>
       )}
-    </div>
+    </Navbar>
   )
 }
 
